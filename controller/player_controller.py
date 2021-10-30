c299d7ca169af041196b6598500f39e779a9a42f
benedekhalaj <bendihalaj@gmail.com>
2021-10-30 19:05:37 +0200

    Player Controller: Refactor check screen boundaries

--- controller/player_controller.py
+++ controller/player_controller.py
@@ -11,16 +11,27 @@ def get_player():
     return player.Player(view.RED)
 
 
+def check_screen_boundaries(player, screen, direction):
+    if direction == "left":
+        return player.x > screen.x
+    elif direction == "right":
+        return player.x + player.width < screen.x + screen.width
+    elif direction == "up":
+        return player.y > screen.y
+    elif direction == "down":
+        return player.y + player.height < screen.y + screen.height
+
+
 def control_player(player, screen):
     keys = view.get_input()
 
-    if keys[pygame.K_LEFT] and player.x > screen.x:
+    if keys[pygame.K_LEFT] and check_screen_boundaries(player, screen, "left"):
         player.move_left()
-    if keys[pygame.K_RIGHT] and player.x + player.width < screen.x + screen.width:
+    if keys[pygame.K_RIGHT] and check_screen_boundaries(player, screen, "right"):
         player.move_right()
-    if keys[pygame.K_UP] and player.y > screen.y:
+    if keys[pygame.K_UP] and check_screen_boundaries(player, screen, "up"):
         player.move_up()
-    if keys[pygame.K_DOWN] and player.y + player.height < screen.y + screen.height:
+    if keys[pygame.K_DOWN] and check_screen_boundaries(player, screen, "down"):
         player.move_down()
     if keys[pygame.K_LSHIFT]:
         player.sprint()
