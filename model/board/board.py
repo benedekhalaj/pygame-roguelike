4e471657562f92ff8ea8a6ab0d65ae53def6b999
dudaskobende <dudaskobende@gmail.com>
2021-11-02 15:27:43 +0100

    Board: fix floor

--- model/board/board.py
+++ model/board/board.py
@@ -1,8 +1,5 @@
-from types import coroutine
 from model import data_manager, util
-import pygame
 
-from model.player.player import Player
 
 def create_map():
     text_map = data_manager.file_opener("model/board/map_file/map.txt")
@@ -21,28 +18,25 @@ def create_board(screen_size):
     map_width = character_width * len(text_map)
     map_height = character_height * len(text_map[0])
     player_position = find_player_position(text_map)
-
-
     asterix = []
-
     for row_place, line in enumerate(text_map):
         for col_place, char in enumerate(line):
             y = ((row_place - player_position[1]) * character_height) + (screen_size[1] / 2 - character_height / 2)
             x = ((col_place - player_position[0]) * character_width) + (screen_size[0] / 2 - character_width / 2)
             position = (x, y, character_width, character_height)
             picture_name = map_sign_dict[char]
+            floor = util.Floor(position, util.WHITE)
+            asterix.append(floor)
             if char == "x":
-                asterix.append(util.Player(position, util.RED))
+                player_on_board = util.Player(position, util.RED)
             elif char == "0":
                 asterix.append(util.Wall(position, util.BLUE))
             elif char == "1":
                 asterix.append(util.Wall(position, util.YELLOW))
             elif char == "2":
                 asterix.append(util.Chest(position, util.GREEN))
-            elif char == " ":
-                asterix.append(util.Floor(position, util.WHITE))
+    asterix.append(player_on_board)
     return asterix
-                
 # pygame.image.load(f"model/board/textures/{picture_name}.png")
 
 
