abc3bed2b7537db4b8a021d5d015dc29f83d2d13
benedekhalaj <bendihalaj@gmail.com>
2021-11-02 14:34:05 +0100

    Global: Add screen rolling

--- controller/player_controller.py
+++ controller/player_controller.py
@@ -1,2 +1,21 @@
 from model.player import player
 from view import view
+import pygame
+
+
+def control_player(asterix):
+    for item in asterix:
+        if item.type == 'player':
+            player_character = item
+
+    keys = view.get_input()
+
+    if keys[pygame.K_LEFT]:
+        player_character.move_left(asterix)
+    if keys[pygame.K_RIGHT]:
+        player_character.move_right(asterix)
+    if keys[pygame.K_UP]:
+        player_character.move_up(asterix)
+    if keys[pygame.K_DOWN]:
+        player_character.move_down(asterix)
+
--- model/util.py
+++ model/util.py
@@ -12,21 +12,44 @@ class Player():
     def __init__(self, position: tuple, color):
         self.rect = pygame.Rect(position[0], position[1], position[2], position[3])
         self.color = color
+        self.velocity = 10
+        self.type = 'player'
+
+    def move_left(self, asterix):
+        for item in asterix:
+            item.rect.x += self.velocity
+
+    def move_right(self, asterix):
+        for item in asterix:
+            item.rect.x -= self.velocity
+
+    def move_up(self, asterix):
+        for item in asterix:
+            item.rect.y += self.velocity
+
+    def move_down(self, asterix):
+        for item in asterix:
+            item.rect.y -= self.velocity
+
+
 
 
 class Wall():
     def __init__(self, position, color):
         self.rect = pygame.Rect(position[0], position[1], position[2], position[3])
         self.color = color
+        self.type = 'wall'
 
 
 class Chest():
     def __init__(self, position, color):
         self.rect = pygame.Rect(position[0], position[1], position[2], position[3])
         self.color = color
+        self.type = 'chest'
 
 
 class Floor():
     def __init__(self, position, color):
         self.rect = pygame.Rect(position[0], position[1], position[2], position[3])
         self.color = color
+        self.type = 'floor'
