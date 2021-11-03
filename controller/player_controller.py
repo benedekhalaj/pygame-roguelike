a81274cc0bb9debde03252720713f3e442a92a92
benedekhalaj <bendihalaj@gmail.com>
2021-11-03 16:33:50 +0100

    Player Controller: Add functions

--- controller/player_controller.py
+++ controller/player_controller.py
@@ -1,2 +1,21 @@
 from model.player import player
 from view import view
+import pygame
+
+
+def control_player(objects):
+    screen_size = (view.WINDOW_WIDTH, view.WINDOW_HEIGHT)
+    for object in objects:
+        if object.type == 'player':
+            player_character = object
+
+    keys = view.get_input()
+
+    if keys[pygame.K_LEFT]:
+        player_character.move(objects, -player_character.velocity, 0, screen_size)
+    if keys[pygame.K_RIGHT]:
+        player_character.move(objects, player_character.velocity, 0, screen_size)
+    if keys[pygame.K_UP]:
+        player_character.move(objects, 0, -player_character.velocity, screen_size)
+    if keys[pygame.K_DOWN]:
+        player_character.move(objects, 0, player_character.velocity, screen_size)
