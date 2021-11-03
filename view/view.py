c8558e06f039e8a179d104bf8edf9824cf2bab3b
benedekhalaj <bendihalaj@gmail.com>
2021-11-03 16:31:39 +0100

    View: Add functions

--- view/view.py
+++ view/view.py
@@ -1,7 +1,7 @@
 import pygame
 
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
 
 def set_display_caption(caption):
@@ -10,3 +10,21 @@ def set_display_caption(caption):
 
 def set_display_mode():
     return pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+def display_objects(window, objects):
+    for item in objects:
+        position = (item.rect.x, item.rect.y, item.rect.width, item.rect.height)
+        pygame.draw.rect(window, item.color, position)
+
+
+def refresh_display():
+    pygame.display.update()
+
+
+def display_background(window):
+    window.fill((0, 0, 0))
+
+
+def get_input():
+    return pygame.key.get_pressed()
