63e617598a5d7e64c4749829f7df97edfa405372
dudaskobende <dudaskobende@gmail.com>
2021-11-12 08:23:16 +0100

    Map: Fix player generate error

--- controller/map_controller.py
+++ controller/map_controller.py
@@ -10,21 +10,22 @@ def level_controller(objects, level, new_level, generate_player):
         save_map(objects, level)
         level = 1
         new_level = True
-        if generate_player:
-            generate_player = False
+        generate_player = False
     elif keys[pygame.K_2]:
         save_map(objects, level)
         level = 2
         new_level = True
+        generate_player = False
     elif keys[pygame.K_3]:
         save_map(objects, level)
         level = 3
         new_level = True
+        generate_player = False
     return new_level, level, generate_player
 
 
-def get_objects(level, unlocked_levels, objects, generaet_player):
-    game = map.Game(objects, (view.WINDOW_WIDTH, view.WINDOW_HEIGHT), view.COLORS, level, generaet_player)
+def get_objects(level, unlocked_levels, objects, generate_player):
+    game = map.Game(objects, (view.WINDOW_WIDTH, view.WINDOW_HEIGHT), view.COLORS, level, generate_player)
     if unlocked_levels < level:
         objects = game.create_map()
         unlocked_levels = level
